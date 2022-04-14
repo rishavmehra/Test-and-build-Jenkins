e0bcf5000c6d4a2c60efbaed3b60ededdb9b963b
Rishav Mehra <rishavmehra61@gmail.com>
2022-04-14 10:10:03 +0530

    installation

---
+++ jenkins-installation.sh
@@ -0,0 +1,25 @@
+# installing jenkins using docker in ubuntu20.04
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo su -
+systemctl enable docker
+systemctl start docker
+
+
+# install Jenkins
+
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
