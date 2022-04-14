99feed8f088670dd56b00d42bfe8e6e654ba6d5e
Rishav Mehra <rishavmehra61@gmail.com>
2022-04-14 16:26:03 +0530

    fixes

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/rishavmehra/fastifyPostgres.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('rishavmehra61@gmail.com')
         }
